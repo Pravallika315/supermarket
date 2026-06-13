0f4ac7430061d1ad81e0c4060c654aea263df73c
Pravallika315 <uddandumpravallika2025@gmail.com>
2026-06-13 15:12:13 +0530

    Added GET routes

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const { createOrder } = require('../controllers/orderController');
+const Order = require('../models/Order');
+
 const router = express.Router();
 
 router.post('/', createOrder);
+
+router.get('/', async (req, res) => {
+  const orders = await Order.find();
+  res.json(orders);
+});
 
 module.exports = router;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const { createUser } = require('../controllers/userController');
+const User = require('../models/User');
+
 const router = express.Router();
 
 router.post('/', createUser);
+
+router.get('/', async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
 
 module.exports = router;
